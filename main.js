6c748bc4585d888487cec87b570af5f340c75b99
fathiyarima <111724404+fathiyarima@users.noreply.github.com>
2023-12-22 23:29:27 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$('.header').height($(window).height ());
+});
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+	event.preventDefault();
+
+	$('html, body').animate({
+		scrollTop: $($.attr(this, 'href')).offset().top
+	}, 800);
+});
+
+$(function(){
+    $('.to_top').hide().on('click', function(){
+        $('body,html').animate({scrollTop : 0}, 800);
+    });
+    $(window).on('scroll', function(){
+        if($(this).scrollTop() > 50){
+            $('.to_top').show();
+        }else{
+            $('.to_top').hide();
+        }
+    });
+});
